7c713736993c59bb9049cacb8ff1eec5adeeb6f5
manhdc06 <manhdc612@gmail.com>
2025-03-14 10:41:59 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Add Netlify redirects file
         </p>
         <a
           className="App-link"
